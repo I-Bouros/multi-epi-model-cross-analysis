d7ee66ace07343b06578b09dc32de188fae77ffc
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-03-11 12:17:11 +0000

    import initial commits

---
+++ epi-models/version_info.py
@@ -0,0 +1,24 @@
+#
+# Version information for pkmodel.
+#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+# See: https://packaging.python.org/guides/single-sourcing-package-version/
+#
+# This file is part of BRANCHPRO
+# (https://github.com/SABS-R3-Epidemiology/branchpro.git) which is released
+# under the BSD 3-clause license. See accompanying LICENSE.md for copyright
+# notice and full license details.
+#
+# Version as a tuple (major, minor, revision)
+#  - Changes to major are rare
+#  - Changes to minor indicate new features, possible slight backwards
+#    incompatibility
+#  - Changes to revision indicate bugfixes, tiny new features
+VERSION_INT = 0, 0, 1
+
+# String version of the version number
+VERSION = '.'.join([str(x) for x in VERSION_INT])
