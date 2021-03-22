68389e563ad1a64ff3b984e35be3ca4f7e0ee7d1
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-03-22 10:22:06 +0000

    initial commit

---
+++ epimodels/phe_model.py
@@ -0,0 +1,17 @@
+#
+# PheSEIRModel Class
+#
+# This file is part of EPIMODELS
+# (https://github.com/I-Bouros/multi-epi-model-cross-analysis.git) which is
+# released under the MIT license. See accompanying LICENSE for copyright
+# notice and full license details.
+#
+"""
+"""
+
+
+class PheSEIRModel():
+    """
+    """
+    def __init__(self):
+        pass
