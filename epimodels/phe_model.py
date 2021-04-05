e77e49f610c63f274c0b7ffdd0f8c042f67dc483
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-04-05 15:42:51 +0100

    Update docstring

--- epimodels/phe_model.py
+++ epimodels/phe_model.py
@@ -34,10 +34,10 @@ class PheSEIRModel(object):
     compartments of the SEIR model.
 
     The general SEIR Model has four compartments:
-     * susceptible individuals (:math:`S`),
-     * exposed but not yet infectious (:math:`E`),
-     * infectious (:math:`I`) and
-     * recovered (:math:`R`).
+        * susceptible individuals (:math:`S`),
+        * exposed but not yet infectious (:math:`E`),
+        * infectious (:math:`I`) and
+        * recovered (:math:`R`).
 
     In the PHE model framework, the exposed and infectious compartments
     .. math::
