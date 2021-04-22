d1911f6a170bbdac1705771fcdfc8fc7151b29e8
Ioana B <ioana.bouros@dtc.ox.ac.uk>
2021-04-22 17:01:21 +0100

    initial commit for the prior method

--- epimodels/phe_model.py
+++ epimodels/phe_model.py
@@ -19,7 +19,7 @@ matrices.
 from itertools import chain
 
 import numpy as np
-from scipy.stats import nbinom, binom
+from scipy.stats import nbinom, binom, gamma, beta
 from scipy.integrate import solve_ivp
 
 import epimodels as em
@@ -821,3 +821,52 @@ class PheSEIRModel(object):
                 p=prob)
 
         return log_lik_pos
+
+    def set_prior(
+            self, prior_dispersion, prior_dI, prior_contact_mult,
+            prior_exp_growth, prior_I0, prior_fatality_ratio,
+            prior_sens, prior_spec, prior_week_var, prior_dL,
+            prior_incubation, prior_time_to_death):
+        """
+        Sets the priors for the parameters used by the PheSEIRModel.
+        """
+        # Prior function of dispersion (e.g. uniformative)
+        a, scale = prior_dispersion
+        self.dispersion_prior = gamma(a, scale=scale)
+
+        # Prior function of mean infectious period dI (e.g. Li et al., 2020)
+        a, loc, scale = prior_dI
+        self.dI_prior = gamma(a, loc=loc, scale=scale)
+
+        # Prior function od all contact matrix multipliers (e.g. uniformative)
+        a, scale = prior_contact_mult
+        self.contact_mult_prior = gamma(a, scale=scale)
+
+        # Prior function of exponential growth - used for computing R0
+        a, scale = prior_exp_growth
+        self.growth_prior = gamma(a, scale=scale)
+
+        #  Prior function of I0
+        # 'to be described'
+
+        # Prior function of infection fatality ratios (e.g. Verity et al.,
+        # 2020)
+        self.fatality_ratio_prior = prior_fatality_ratio
+
+        # Prior function for serological test sensitivity
+        a, b = prior_sens
+        self.sens_prior = beta(a, b)
+
+        # Prior function for serological test specificity
+        a, b = prior_spec
+        self.spec_prior = beta(a, b)
+
+        #  Prior function of weekly variation in transmission (e.g.
+        #  informative)
+        a, scale = prior_week_var
+        self.week_var_prior = gamma(a, scale=scale)
+
+        # Fixed mean latent period
+        self.dL = prior_dL
+
+        # Fixed incubation period
